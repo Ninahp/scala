340131e8d72137b192e05861e05c211ad48e007d
TheBeachmaster <otienoken@outlook.com>
2017-12-19 16:12:33 +0300

    Purge unwanted Scala files

--- mqtt_alpakka/hello-alpakka/src/main/scala/Main.scala
+++ mqtt_alpakka/hello-alpakka/src/main/scala/Main.scala
@@ -1,3 +1,24 @@
 object Main extends App {
   println("Hello, World!")
+  val connectionSettings = MqttConnectionSettings(
+    "tcp://sungura1-angani-host-ke.africastalking.com:1882",
+    "alpakka-client",
+    new MemoryPersistence
+  )
+  val topic1 = "alpakka/client/topic1"
+  val topic2 = "alpakka/client/topic2"
+  val bufferSize = 8
+  val settings = MqttSourceSettings(
+    conncetionSettings.withClientId("alpakka-client-1"),
+    conncetionSettings.withAuth("alpakka","alpakka"),
+    Map(topic1 -> MqttQos.AtleastOnce, topic2 -> MqttQos.AtleastOnce)
+  )
+  val mqttSource = mqttSource(settings, bufferSize)
+  val (subscriptionFuture, result) = mqttSource
+  .map(m => s"${m.topic}_${m.payload.utf8String}")
+  .take(messaGeCount * 2)
+  .toMat(Sink.seq)(Keep.both)
+  .run()
+  Source(messages).runWith(MqttSink(connectionSettings.withClientId("alpakka/sink"), MqttQoS.AtLeastOnce))
+  
 }
