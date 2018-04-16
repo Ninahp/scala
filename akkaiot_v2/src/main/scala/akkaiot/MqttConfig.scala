6abd4b6035da7946e389e1eeaac163a60ec0534c
TheBeachmaster <otienoken@outlook.com>
2018-04-16 16:22:57 +0300

    Auth

--- akkaiot_v2/src/main/scala/akkaiot/MqttConfig.scala
+++ akkaiot_v2/src/main/scala/akkaiot/MqttConfig.scala
@@ -16,9 +16,9 @@ object MqttConfig {
 
   // Pub-Sub config
   val psConfig = PSConfig(
-    brokerUrl = "tcp://sungura1-angani-ke-host.africastalking.com:1883",
-    userName = "sandbox",
-    password = "sandbox",
+    brokerUrl = "tcp://sungura1-angani-ke-host.africastalking.com:10883",
+    userName = "akka",
+    password = "akkapass",
     stashTimeToLive = 1.minute,
     stashCapacity = 8000,
     reconnectDelayMin = 10.millis,
